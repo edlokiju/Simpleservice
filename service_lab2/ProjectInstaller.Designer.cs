813264483c80f329d966c8d591b9625101f68100
edlokiju <edlokiju@gmail.com>
2023-04-03 14:21:51 +0700

    Add project files.

---
+++ service_lab2/ProjectInstaller.Designer.cs
@@ -0,0 +1,68 @@
+﻿using System.ServiceProcess;
+namespace service_lab2
+{
+    partial class ProjectInstaller
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.serviceProcessInstaller1 = new System.ServiceProcess.ServiceProcessInstaller();
+            this.serviceInstaller1 = new System.ServiceProcess.ServiceInstaller();
+            // 
+            // serviceProcessInstaller1
+            // 
+            this.serviceProcessInstaller1.Account = System.ServiceProcess.ServiceAccount.LocalSystem;
+            this.serviceProcessInstaller1.Password = null;
+            this.serviceProcessInstaller1.Username = null;
+            // 
+            // serviceInstaller1
+            // 
+            this.serviceInstaller1.Description = "Lab 2 - Bai 1.4";
+            this.serviceInstaller1.DisplayName = "_lab1-b.1.1.4";
+            this.serviceInstaller1.ServiceName = "_lab1-b.1.1.4";
+            this.serviceInstaller1.StartType = ServiceStartMode.Automatic;
+            //
+            // after service is installed, start it automically 
+            this.serviceInstaller1.AfterInstall += (s, e) =>
+            {
+                ServiceInstaller serviceInstaller = (ServiceInstaller)s;
+                (new ServiceController(serviceInstaller.ServiceName)).Start();
+            };
+            // 
+            // ProjectInstaller
+            // 
+            this.Installers.AddRange(new System.Configuration.Install.Installer[] {
+            this.serviceProcessInstaller1,
+            this.serviceInstaller1});
+
+        }
+
+        #endregion
+
+        private System.ServiceProcess.ServiceProcessInstaller serviceProcessInstaller1;
+        private System.ServiceProcess.ServiceInstaller serviceInstaller1;
+    }
+}
---
+++ service_lab2/Service1.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.InteropServices;
+using System.Threading;
+using static System.Collections.Specialized.BitVector32;
+
+namespace service_lab2
+{
+    public partial class Service1 : ServiceBase
+    {
+        public Service1()
+        {
+            this.CanHandleSessionChangeEvent = true;
+            InitializeComponent();
+        }
+
+        // define
+
+        [DllImport("wtsapi32.dll", SetLastError = true)]
+        private static extern bool WTSSendMessage(
+            IntPtr hServer,
+            [MarshalAs(UnmanagedType.I4)] int SessionId,
+            string pTitle,
+            [MarshalAs(UnmanagedType.U4)] int TitleLength,
+            string pMessage,
+            [MarshalAs(UnmanagedType.U4)] int MessageLength,
+            [MarshalAs(UnmanagedType.I4)] int Style,
+            [MarshalAs(UnmanagedType.I4)] int Timeout,
+            [MarshalAs(UnmanagedType.I4)] out int pResponse,
+            bool bWait
+        );
+
+        private const int MB_OK = 0x00000000;
+        private const int MB_OKCANCEL = 0x00000001;
+        private const int MB_YESNO = 0x00000004;
+        public static bool ShowMessage(string title, string message)
+        {
+            int sessionId = WTSGetActiveConsoleSessionId();
+            int response;
+            return WTSSendMessage(
+                IntPtr.Zero,
+                sessionId,
+                title,
+                title.Length,
+                message,
+                message.Length,
+                MB_OK,
+                0,
+                out response,
+                true
+            );
+        }
+
+        [DllImport("kernel32.dll")]
+        private static extern int WTSGetActiveConsoleSessionId();
+
+
+        protected override void OnSessionChange(SessionChangeDescription changeDescription)
+        {
+            if (
+                changeDescription.Reason == SessionChangeReason.SessionLogon
+                || changeDescription.Reason == SessionChangeReason.SessionUnlock
+            )
+            {
+                Thread t = new Thread(() =>
+                {
+                    try
+                    {
+                        ShowMessage("20251635", "Login Sucess");
+                    }
+                    catch { }
+                });
+                t.SetApartmentState(ApartmentState.STA);
+                t.Start();
+
+            }
+            base.OnSessionChange(changeDescription);
+        
+        }
+    protected override void OnStart(string[] args)
+        {
+        }
+
+    protected override void OnStop()
+        {
+        }
+    }
+}
